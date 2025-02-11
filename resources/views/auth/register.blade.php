82e4f8362d11c560caf4c3ae8f6b55c4615d600e
Vivek Lode <vivek.lode1@gmail.com>
2025-02-11 17:58:30 +0530

    Blade Components and route created
    Blade Components and route created

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-page-heading>
+        Register
+    </x-page-heading>
+    <x-forms.form method="POST" action="/register">
+        <x-forms.input name="name" label="Your Name" />
+    </x-forms.form>
+</x-layout>
